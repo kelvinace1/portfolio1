027efcff16802cb8dba7eb87abd3ca6c8317d7cc
kelvinace1 <72827998+kelvinace1@users.noreply.github.com>
2020-11-12 12:03:05 -0800

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,11 @@
+(function(){
+    var dialog = document.getElementById('dial');
+    document.getElementById('show').onclick=function(){
+        dialog.show()
+    };
+    document.getElementById('hide').onclick=function(){
+        dialog.close()
+    }
+
+})();
+
